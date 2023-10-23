0aeff47128e95877d3498e675bd685b977d5c0c6
MarozsanMate <mateking0508@gmail.com>
2023-10-23 17:01:19 +0200

    Initial commit

---
+++ model.py
@@ -0,0 +1,70 @@
+from functools import total_ordering
+
+
+@total_ordering
+class Filmek:
+    cim: str
+    __hossz: int
+    _korhat: bool
+
+    def __init__(self, cim, hossz, korhat):
+        self.cim = cim
+        self.__hossz = hossz
+        self._korhat = korhat
+
+    @property
+    def hossz(self):
+        return self.__hossz
+
+    @hossz.setter
+    def hossz(self, ert):
+        self.__hossz = ert
+
+    @property
+    def korhat(self):
+        return self._korhat
+
+    @korhat.setter
+    def korhat(self, ert):
+        self._korhat = ert
+
+    def __repr__(self):
+        return f'{self.cim}, {self.__hossz}, {self._korhat}'
+
+    def __str__(self):
+        return f"{self.cim}: {self.__hossz} perces, {'korhatáros' if self._korhat else 'nem korhatáros'}"
+
+    def __eq__(self, other):
+        if not isinstance(other, Filmek):
+            return NotImplemented('Nem létezik ilyen!')
+        return self.cim == other.cim and self.__hossz == other.__hossz and self._korhat == other._korhat
+
+    def __lt__(self, other):
+        if self.__hossz != other.__hossz:
+            return self.__hossz > other.__hossz
+        if self._korhat != other._korhat:
+            return self._korhat < other._korhat
+        return self.cim < other.cim
+
+    @staticmethod
+    def Film_L_H(filmek, hossz):
+        ls = []
+        for i in filmek:
+            if i.__hossz == hossz:
+                ls.append(i.cim)
+        return ls
+
+
+class Sorozatok(Filmek):
+    __ep: int
+
+    def __int__(self, cim, hossz, ep, korhat):
+        super().__init__(cim, hossz, korhat)
+        self.__ep = ep
+
+    def __repr__(self):
+        return f'{self.cim}, {self.__hossz}, {self._korhat}, {self.__ep}'
+
+    def __str__(self):
+        return f"{self.cim}: {self.__hossz} perces, {'korhatáros' if self._korhat else 'nem korhatáros'}, {self.__ep} részes"
+
---
+++ test.py
@@ -0,0 +1,25 @@
+import sys
+from model import Filmek,Sorozatok
+if __name__ == '__main__':
+    try:
+        if (len(sys.argv) < 2): raise ValueError
+        n = int(sys.argv[1])
+        if not sys.argv[1].isnumeric():
+            raise ValueError
+        if(n <= 0):
+            raise ValueError
+    except Exception as exp:
+        print(exp)
+    n = int(sys.argv[1])
+    film = []
+    for _ in range(n):
+        c = input().split(';')
+        kh = c[2] == 'True'
+        if (len(c)==3):
+            film.append(Filmek(c[0],int(c[1]), kh))
+        elif(len(c)==4):
+            film.append(Sorozatok(c[0],int(c[3]),kh))
+        raise ValueError
+    film.sort()
+    for f in film:
+        print(f)
